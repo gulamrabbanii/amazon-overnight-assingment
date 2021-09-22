46f994e42b785b7a46bb7795e5ff7487932f8b47
gulamrabbanii <43692157+gulamrabbanii@users.noreply.github.com>
2021-09-22 17:53:04 +0530

    Update findKey.c

--- findKey.c
+++ findKey.c
@@ -1,38 +1,37 @@
-//  In an array that is sorted, but rotated, find the position of the given number in O(log n).
-
-/* 
-eg:
-Input array : [3, 10, 20, -3, 2]
-Input number : -3
-Output : Index position : 3
-*/
-
 #include <stdio.h>
 
-int findNumber(int *arr, int low, int high, int key)
+int search(int arr[], int l, int h, int key)
 {
-    if(low == high){
-        if(arr[low] == key)
-            return low;
-        return -1;
-    }
-    int mid = low + (high - low)/2;
-    int j = findNumber(arr, low, mid, key);
-    if(j != -1)
-        return j;
-    
-    int k = findNumber(arr, mid+1, high, key);
-    if(k != -1)
-        return k;
-    return -1;
-}    
+	if (l > h)
+		return -1;
+
+	int mid = (l + h) / 2;
+	if (arr[mid] == key)
+		return mid;
+
+	if (arr[l] <= arr[mid]) {
+
+		if (key >= arr[l] && key <= arr[mid])
+			return search(arr, l, mid - 1, key);
+			
+		return search(arr, mid + 1, h, key);
+	}
+
+	if (key >= arr[mid] && key <= arr[h])
+		return search(arr, mid + 1, h, key);
+
+	return search(arr, l, mid - 1, key);
+}
+
 int main()
 {
-    int key = -3;
-    int arr[10] = {3, 2, 1,-3, 4};
-    int n = sizeof(arr)/sizeof(arr[0]);
-    
-    printf("index is %d\n", findNumber(arr, 0, n-1, key));
+	int arr[] = { 4, 5, 6, 7, 8, 9, 1, 2, 3 };
+	int n = sizeof(arr) / sizeof(arr[0]);
+	int key = 6;
+	int i = search(arr, 0, n - 1, key);
 
-    return 0;
+	if (i != -1)
+		printf("Index is %d", i);
+	else
+		printf("Key not found");
 }
